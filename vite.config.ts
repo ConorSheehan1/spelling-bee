09e6c5e80a47cfb2158ad86dc87a26cda5b8bc9f
Conor Sheehan <conor.sheehan.dev@gmail.com>
2022-09-06 22:57:02 +0100

    build-cache-bust (#11)
    * add date to build outputs so cache is busted correctly.
    
    previously had issues with browsers caching js, css, favicons etc. too
    long
    
    * linting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import vue from "@vitejs/plugin-vue";
 import { vueI18n } from "@intlify/vite-plugin-vue-i18n";
 import checker from "vite-plugin-checker";
 
+// https://stackoverflow.com/a/68123263/6305204
+// ensure cache is busted correctly on redeploy
+const dateHash = new Date().toISOString().split("T")[0];
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -13,4 +17,13 @@ export default defineConfig({
       include: path.resolve(__dirname, "./src/locales/**"),
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name]-${dateHash}.js`,
+        chunkFileNames: `[name]-${dateHash}.js`,
+        assetFileNames: `[name]-${dateHash}.[ext]`,
+      },
+    },
+  },
 });
